b9c919803126eb5ba1e3c9067fa618b446e45917
zlatan <zlatan.pham@gmail.com>
2019-03-28 18:29:11 +0700

    support tailwindcss

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ This boilerplate using `react-app-rewired` helps to override `create-react-app` 
 - set alias `@` for `src`.
 - bind `react-hot-loader`.
 - `styled-component` & its babel plugin
+- support tailwindcss & its macros for styled-components
--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,39 @@
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+const path = require('path');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+const glob = require('glob-all');
 const {
   addWebpackResolve,
-  override,
   addBabelPlugin,
+  override,
+  addPostcssPlugins,
 } = require('customize-cra');
-const path = require('path');
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
 
 module.exports = override(
-  addBabelPlugin('styled-components'),
   addWebpackResolve({ alias: { '@': path.resolve(__dirname, 'src') } }),
+  addBabelPlugin('babel-plugin-styled-components'),
+  addPostcssPlugins([require('tailwindcss')('./src/tailwind.js')]),
+  config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins = (config.plugins || []).concat([
+        new PurgecssPlugin({
+          paths: glob.sync([path.join(__dirname, 'src/**/*.js')]),
+          extractors: [
+            {
+              extractor: TailwindExtractor,
+              extensions: ['jsx', 'js'],
+            },
+          ],
+        }),
+      ]);
+    }
+    return config;
+  },
   config => rewireReactHotLoader(config),
 );
